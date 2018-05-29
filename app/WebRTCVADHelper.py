80a9add9e144f7166cedb76426bf8fd4c91988a6
Christopher Bain <csbain@gmail.com>
2018-05-29 14:49:26 +1000

    changed vad agressiveness

--- app/WebRTCVADHelper.py
+++ app/WebRTCVADHelper.py
@@ -14,7 +14,7 @@ class WebRTCVADHelper:
     sr_segment_list = []
 
     def __init__(self, temp_file_helper, import_wav_path):
-        self.vad = webrtcvad.Vad(2)  # agressiveness
+        self.vad = webrtcvad.Vad(3)  # agressiveness
         self.sample_rate = SharedParams.SAMPLE_RATE
         self.pcm_data = None
         self.temp_file_helper = temp_file_helper
