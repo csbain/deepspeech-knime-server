2b899b0f88e6e9f13fa99b352a54d5a151cdba4d
Christopher Bain <csbain@gmail.com>
2018-06-08 08:31:07 +1000

    fix

--- app/benchmark.py
+++ app/benchmark.py
@@ -52,7 +52,7 @@ def run_simulation(vad, processing_type):
 vads = [0,1,2,3]
 processing_types = ['multiprocessor','singlethreaded']
 parser = argparse.ArgumentParser(description='Run benchmarks on Deepseech integration')
-parser.add_argument('-v', '--vad', choices=vads, required=True)
+parser.add_argument('-v', '--vad', choices=vads, type=int, required=True)
 parser.add_argument('-p', '--processing_type', choices=processing_types, required=True)
 args = parser.parse_args()
 
